102e7362a485a06895750b2300afe057337b6508
Daniel Parreira <daniel@Daniels-MacBook-Pro.local>
2017-04-26 00:34:48 -0300

    Incrementar setup

--- setup.sh
+++ setup.sh
@@ -3,20 +3,22 @@
 
 echo ">>> Setup em processo!"
 
-for app in AdminApp, CadastroAppAPI, CadastroAppFrontEnd, Documentacao; do
+echo ">>> Clonando repositorios"
+for app in AdminApp CadastroAppAPI CadastroAppFrontEnd Documentacao; do
 		git clone "git@github.com:ProjetoIncluirUFMG/$app.git" "../$app"
 done
 
+echo ">>> Destruindo containers existentes"
 docker-compose down
 
+echo ">>> Instalando dependencias para as aplicaçōes"
 /bin/bash ./database/install_dependencies.sh
 
+echo ">>> Criando containers para as aplicaçōes"
 docker-compose up -d --build
 
 echo ">>> Recuperando banco de dados..."
-
 sleep 30
-
 /bin/bash ./database/restore_backup.sh ./database/backup.sql
 
 echo ">>> Recuperação concluida!"
