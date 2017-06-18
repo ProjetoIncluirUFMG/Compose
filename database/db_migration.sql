e93e0514c751914f3a8355921ad61f8695d280f0
Daniel Marchena Parreira <danielmapar@gmail.com>
2017-06-18 20:24:16 -0300

    Consertar migracao da tabela aluno

--- database/db_migration.sql
+++ database/db_migration.sql
@@ -1,4 +1,4 @@
 ALTER TABLE aluno
 ADD COLUMN google_places_json VARCHAR(10000) AFTER email,
-ADD COLUMN endereco_google VARCHAR(10000) AFTER google_places_json,
+ADD COLUMN endereco_google VARCHAR(255) AFTER google_places_json,
 ADD COLUMN cadastro_atualizado TINYINT(1) AFTER id_aluno
