0c78daf7a85b30a8dbccc5ecc7ebb90ce2308f84
Daniel Marchena Parreira <danielmapar@gmail.com>
2017-06-18 04:18:42 -0300

    Adicionar migracao do banco de dados

---
+++ database/db_migration.sql
@@ -0,0 +1,4 @@
+ALTER TABLE aluno
+ADD COLUMN google_places_json VARCHAR(10000) AFTER email,
+ADD COLUMN endereco_google VARCHAR(10000) AFTER google_places_json,
+ADD COLUMN cadastro_atualizado TINYINT(1) AFTER id_aluno
