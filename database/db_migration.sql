1b40e2132476e1353c94babe1e6228a8c021752b
Daniel Marchena Parreira <danielmapar@gmail.com>
2017-06-21 02:31:02 -0300

    Adicionar coluna de senha na migracao

--- database/db_migration.sql
+++ database/db_migration.sql
@@ -1,4 +1,5 @@
 ALTER TABLE aluno
-ADD COLUMN google_places_json VARCHAR(10000) AFTER email,
+ADD COLUMN senha VARCHAR(60) AFTER email,
+ADD COLUMN google_places_json VARCHAR(10000) AFTER senha,
 ADD COLUMN endereco_google VARCHAR(255) AFTER google_places_json,
 ADD COLUMN cadastro_atualizado TINYINT(1) AFTER id_aluno
