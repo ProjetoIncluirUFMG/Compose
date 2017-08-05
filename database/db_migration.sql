1d6e894eafff9aa725050a8a6cfd233bda22965f
Daniel Marchena Parreira <danielmapar@gmail.com>
2017-08-05 10:07:02 -0300

    Adicionar coluna de usuario

--- database/db_migration.sql
+++ database/db_migration.sql
@@ -2,4 +2,5 @@ ALTER TABLE aluno
 ADD COLUMN senha VARCHAR(60) AFTER email,
 ADD COLUMN google_places_json VARCHAR(10000) AFTER senha,
 ADD COLUMN endereco_google VARCHAR(255) AFTER google_places_json,
-ADD COLUMN cadastro_atualizado TINYINT(1) AFTER id_aluno
+ADD COLUMN cadastro_atualizado TINYINT(1) AFTER id_aluno,
+ADD COLUMN recuperar_senha_token VARCHAR(255) AFTER cadastro_atualizado
