3ddfe71de6da2e76159352413d27458f0ebffdc1
Daniel Marchena Parreira <danielmapar@gmail.com>
2017-11-19 23:38:54 -0500

    Consertar migracao de banco

--- database/db_migration.sql
+++ database/db_migration.sql
@@ -2,17 +2,15 @@ ALTER TABLE aluno
 ADD COLUMN senha VARCHAR(60) AFTER email,
 ADD COLUMN google_places_json VARCHAR(10000) AFTER senha,
 ADD COLUMN cadastro_atualizado TINYINT(1) AFTER id_aluno,
-ADD COLUMN recuperar_senha_token VARCHAR(255) AFTER cadastro_atualizado
+ADD COLUMN recuperar_senha_token VARCHAR(255) AFTER cadastro_atualizado;
 
-CREATE TABLE `configuracao_cadastro` (
-  `id` int(1) NOT NULL,
-  `texto_inicial` text NOT NULL,
-  `somente_veterano` tinyint(1) NOT NULL
+CREATE TABLE configuracao_cadastro (
+  id int(1) NOT NULL,
+  texto_inicial text NOT NULL,
+  somente_veterano tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `configuracao_cadastro`
+ALTER TABLE configuracao_cadastro
   ADD PRIMARY KEY (`id`);
-COMMIT;
 
-INSERT INTO `configuracao_cadastro` (`id`, `texto_inicial`, `somente_veterano`) VALUES ('1', 'Texto inicial', '1')
-COMMIT;
+INSERT INTO configuracao_cadastro (`id`, `texto_inicial`, `somente_veterano`) VALUES ('1', 'Texto inicial', '1');
