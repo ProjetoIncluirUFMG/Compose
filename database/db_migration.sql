935a5a5b7d9e149ba40271f9f7d9c62f58607397
Daniel Marchena Parreira <danielmapar@gmail.com>
2017-12-04 01:42:45 -0500

    Adicionar suporte a novas colunas

--- database/db_migration.sql
+++ database/db_migration.sql
@@ -5,12 +5,12 @@ ADD COLUMN cadastro_atualizado TINYINT(1) AFTER id_aluno,
 ADD COLUMN recuperar_senha_token VARCHAR(255) AFTER cadastro_atualizado;
 
 ALTER TABLE disciplina
-ADD COLUMN vagas_do_curso INT(10) DEFAULT 0 AFTER status,
-ADD COLUMN fila_de_nivelamento INT(10) DEFAULT 0 AFTER vagas_do_curso,
-ADD COLUMN fila_de_espera INT(10) DEFAULT 0 AFTER fila_de_nivelamento,
-ADD COLUMN total_vagas_do_curso INT(10) DEFAULT 0 AFTER fila_de_espera,
-ADD COLUMN total_fila_de_nivelamento INT(10) DEFAULT 0 AFTER total_vagas_do_curso,
-ADD COLUMN total_fila_de_espera INT(10) DEFAULT 0 AFTER total_fila_de_nivelamento,
+ADD COLUMN vagas_do_curso INT(10) DEFAULT 3 AFTER status,
+ADD COLUMN fila_de_nivelamento INT(10) DEFAULT 3 AFTER vagas_do_curso,
+ADD COLUMN fila_de_espera INT(10) DEFAULT 3 AFTER fila_de_nivelamento,
+ADD COLUMN total_vagas_do_curso INT(10) DEFAULT 10 AFTER fila_de_espera,
+ADD COLUMN total_fila_de_nivelamento INT(10) DEFAULT 10 AFTER total_vagas_do_curso,
+ADD COLUMN total_fila_de_espera INT(10) DEFAULT 10 AFTER total_fila_de_nivelamento,
 ADD COLUMN idade_minima INT(3) DEFAULT 0 AFTER total_fila_de_espera;
 
 CREATE TABLE configuracao_cadastro (
@@ -25,6 +25,6 @@ ALTER TABLE configuracao_cadastro
 
 INSERT INTO configuracao_cadastro (`id`, `texto_inicial`, `somente_veterano`, `sistema_ativo`) VALUES ('1', 'Texto inicial', '1', '1');
 
-CREATE TABLE pre_matricula ( `numero_comprovante` INT(10) NOT NULL , `status` VARCHAR(45) NOT NULL DEFAULT 'Ativo' , `data_modificado` DATETIME NOT NULL, `aluno_cpf` VARCHAR(15) NOT NULL , `nome_curso` VARCHAR(45) NOT NULL , `id_curso` INT(11) NOT NULL , `nome_disciplina` VARCHAR(100) NOT NULL , `id_disciplina` INT(11) NOT NULL , `nome_turma` VARCHAR(100) NOT NULL , `id_turma` INT(11) NOT NULL , `veterano` BOOLEAN NOT NULL , `vaga_garantida` BOOLEAN NOT NULL , `fila_nivelamento` BOOLEAN NOT NULL , `fila_espera` BOOLEAN NOT NULL , `nome_aluno` VARCHAR(100) NOT NULL , `id_aluno` INT(11) NOT NULL , PRIMARY KEY (`numero_comprovante`)) ENGINE = InnoDB;
+CREATE TABLE pre_matricula ( `numero_comprovante` INT(10) NOT NULL , `turno` TEXT(100) NOT NULL , `status` VARCHAR(45) NOT NULL DEFAULT 'Ativo' , `data_modificado` DATETIME NOT NULL, `aluno_cpf` VARCHAR(15) NOT NULL , `nome_curso` VARCHAR(45) NOT NULL , `id_curso` INT(11) NOT NULL , `nome_disciplina` VARCHAR(100) NOT NULL , `id_disciplina` INT(11) NOT NULL , `nome_turma` VARCHAR(100) NOT NULL , `id_turma` INT(11) NOT NULL , `veterano` BOOLEAN NOT NULL , `vaga_garantida` BOOLEAN NOT NULL , `fila_nivelamento` BOOLEAN NOT NULL , `fila_espera` BOOLEAN NOT NULL , `nome_aluno` VARCHAR(100) NOT NULL , `id_aluno` INT(11) NOT NULL , PRIMARY KEY (`numero_comprovante`)) ENGINE = InnoDB;
 
 ALTER TABLE pre_matricula ADD CONSTRAINT `fk_disciplina` FOREIGN KEY (`id_disciplina`) REFERENCES `disciplina`(`id_disciplina`) ON DELETE NO ACTION ON UPDATE NO ACTION; ALTER TABLE `pre_matricula` ADD CONSTRAINT `fk_aluno` FOREIGN KEY (`id_aluno`) REFERENCES `aluno`(`id_aluno`) ON DELETE NO ACTION ON UPDATE NO ACTION; ALTER TABLE `pre_matricula` ADD CONSTRAINT `fk_curso` FOREIGN KEY (`id_curso`) REFERENCES `curso`(`id_curso`) ON DELETE NO ACTION ON UPDATE NO ACTION; ALTER TABLE `pre_matricula` ADD CONSTRAINT `fk_turma` FOREIGN KEY (`id_turma`) REFERENCES `turma`(`id_turma`) ON DELETE NO ACTION ON UPDATE NO ACTION;
