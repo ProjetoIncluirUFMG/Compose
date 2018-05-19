69ceeb36eec3c4be61d6bc5154185b0396cf32c6
Ariel Santos <as-ariel@outlook.com>
2018-05-19 04:25:15 -0300

    Add dependencias de pacotes do Ubuntu

--- setup.sh
+++ setup.sh
@@ -2,7 +2,25 @@
 
 set -ex
 
-mkdir dev.incluir && cd dev.incluir
+sudo apt-get update -y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common \
+	unzip wget git curl -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+mkdir /dev.incluir && cd /dev.incluir
 wget https://github.com/ProjetoIncluirUFMG/Compose/archive/google-deploy.zip
 unzip google-deploy.zip
 cp -R Compose-google-deploy/* ./ && rm -rf Compose-google-deploy/
